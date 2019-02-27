ff7323fc0f94212d4b32418aa425c8abf6c845f3
Tommy Lau <tlau425@yahoo.com>
2019-02-27 00:12:28 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(array)
   else 
     message = "The line is currently:"
     array.each_with_index do |name, index|
-    message += " #{index + 1}. #{name}."
+    message += " #{index + 1}. #{name}"
   end
   puts "#{message}"
 end
